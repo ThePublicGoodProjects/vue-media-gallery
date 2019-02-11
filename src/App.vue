946416a78a7593d01f41d9c887ba45ed9fafec61
cam-pgp <cam.wiley@publicgoodprojects.org>
2019-02-11 13:34:57 -0800

    app name update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="mhcDownloadGallery">
     <Posts campaign="mental-health-champions" host="https://downloadcenter.actionminded.org" />
   </div>
 </template>
@@ -9,7 +9,7 @@ import './assets/sass/app.scss';
 import Posts from './components/Posts.vue'
 
 export default {
-  name: 'app',
+  name: 'mhcDownloadGallery',
   components: {
     Posts
   },
